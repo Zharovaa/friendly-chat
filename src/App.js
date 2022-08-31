721ab06e4e2065a4c7a184bcdeb5806a4fa5c7c3
dzharova0203@gmail.com <dzharova0203@gmail.com>
2022-08-31 23:47:04 +0300

    working with sign-out and validation functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from "react";
+import React from 'react';
 
 /* Styles */
-import "./assets/scss/main.scss";
+import './assets/scss/main.scss';
 
 /* Components */
-import Navigation from "./components/Navigation";
-import UsersPage from "./pages/Users/";
-import ProfilePage from "./pages/Profile/";
-import SignIn from "./pages/SignIn";
-import MessagesPage from "./pages/Messages/";
+import Navigation from './components/Navigation';
+import UsersPage from './pages/Users/';
+import ProfilePage from './pages/Profile/';
+import SignIn from './pages/SignIn';
+import MessagesPage from './pages/Messages/';
 
 /* Libraries */
-import { Switch, Route } from "react-router-dom";
-import { Container } from "@mui/material";
+import { Switch, Route } from 'react-router-dom';
+import { CircularProgress, Container } from '@mui/material';
+import { useDispatch } from 'redux-react-hook';
+import { getAuthUserData } from './redux/auth-reducer';
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Navigation />
-      <Container fixed sx={{ padding: "30px 0" }}>
+      <Container fixed sx={{ padding: '30px 0' }}>
         <Switch>
           <Route path="/messages/:dialogId?" render={() => <MessagesPage />} />
           <Route path="/profile/:userId?" render={() => <ProfilePage />} />
