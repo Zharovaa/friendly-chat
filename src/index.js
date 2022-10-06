f3339bad5ff0a75fdc6b8792ac750b6ef7bd69e0
Daria Zharova <dzharova0203@gmail.com>
2022-10-06 20:54:01 +0300

    deployed the project

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./redux/redux-store";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 let rerenderEntireTree = (state) => {
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Provider store={store}>
         <App store={store} />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
